8c406b0659efb443a6a9a57a8889650c5e953c6c
Marco Nicola <marconicola@disroot.org>
2022-06-24 21:17:44 +0200

    Decide between SSE/AVX functions only once per function

--- matfuncs/addconst_amd64.go
+++ matfuncs/addconst_amd64.go
@@ -6,20 +6,24 @@
 
 package matfuncs
 
+var (
+	addConst32 = AddConstSSE32
+	addConst64 = AddConstSSE64
+)
+
+func init() {
+	if hasAVX {
+		addConst32 = AddConstAVX32
+		addConst64 = AddConstAVX64
+	}
+}
+
 // AddConst32 adds a constant value c to each element of x, storing the result in y (32 bits).
 func AddConst32(c float32, x, y []float32) {
-	if hasAVX2 {
-		AddConstAVX32(c, x, y)
-		return
-	}
-	AddConstSSE32(c, x, y)
+	addConst32(c, x, y)
 }
 
 // AddConst64 adds a constant value c to each element of x, storing the result in y (64 bits).
 func AddConst64(c float64, x, y []float64) {
-	if hasAVX2 {
-		AddConstAVX64(c, x, y)
-		return
-	}
-	AddConstSSE64(c, x, y)
+	addConst64(c, x, y)
 }
