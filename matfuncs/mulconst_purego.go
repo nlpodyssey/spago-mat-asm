b6b25e3bb5d261054014c22b729e29ec300012e6
Marco Nicola <marconicola@disroot.org>
2022-06-25 18:40:08 +0200

    Create MulConst... functions

---
+++ matfuncs/mulconst_purego.go
@@ -0,0 +1,17 @@
+// Copyright 2022 The NLP Odyssey Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build !amd64 || !gc || purego
+
+package matfuncs
+
+// MulConst32 multiplies each element of x by a constant value c, storing the result in y (32 bits).
+func MulConst32(c float32, x, y []float32) {
+	mulConst(c, x, y)
+}
+
+// MulConst64 multiplies each element of x by a constant value c, storing the result in y (64 bits).
+func MulConst64(c float64, x, y []float64) {
+	mulConst(c, x, y)
+}
